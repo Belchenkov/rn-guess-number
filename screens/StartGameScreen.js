c5f2c2ab90222d8a13703d34523910353ec47a68
Aleksey Belchenkov <u608110@gmail.com>
2020-05-21 15:39:10 +0300

    Confirming User Input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,10 +14,35 @@ import Input from "../components/Input";
 
 const StartGameScreen = () => {
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState(0);
 
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            return;
+        }
+
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+    }
 
     return (
         <TouchableNativeFeedback onPress={() => {
@@ -41,19 +66,20 @@ const StartGameScreen = () => {
                         <View style={styles.button}>
                             <Button
                                 title="Reset"
-                                onPress={() => {}}
+                                onPress={resetInputHandler}
                                 color={colors.accent}
                             />
                         </View>
                         <View style={styles.button}>
                             <Button
                                 title="Confirm"
-                                onPress={() => {}}
+                                onPress={confirmInputHandler}
                                 color={colors.primary}
                             />
                         </View>
                     </View>
                 </Card>
+                { confirmedOutput }
             </View>
         </TouchableNativeFeedback>
     );
