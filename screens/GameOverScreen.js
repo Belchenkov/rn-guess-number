d7f8adf6a437d799dc020cca5fdeba74fadaa2d7
Aleksey Belchenkov <u608110@gmail.com>
2020-05-30 18:17:13 +0300

    Add Icons

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text, Button } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
